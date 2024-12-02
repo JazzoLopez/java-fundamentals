3a4b7cd489ea3e44f08154b5c6bc869bc7cff5a6
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-12-02 17:40:29 -0600

    Add: class type var

--- src/TypeVar.java
+++ src/TypeVar.java
@@ -1,7 +1,16 @@
 public class TypeVar {
     public static void main(String[] args) {
         System.out.println("<-- USO DE VAR EN JAVA -->");
-        var name = "Jazzo";
+        //VARIABLE LOCAL AL METODO
+        var name = "Jazzo"; //EL TIPO DE DATO INFIERE EN BASE A LO QUE ALMACENAMOS, SOLO ALMACENA VALORES CADENA
+        var age = 20; //SE INFIERE EL TIPO INT
         System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        var salary = 50000.5; //SE INFIERE EL TIPO FLOAT
+        System.out.println("salary = " + salary);
+        var isMarried = false;//INFIERE LOS DATOS DE TIPO BOOLEAN
+        System.out.println("isMarried = " + isMarried);
+        isMarried = true; // CORRECTO
+        //var data; //NO SE PUEDE PRIMERO DEFINIR Y LUEGO ASIGNAR
     }
 }
