87bb281d24ed8ac8c4e761ae02f56b746d8f9197
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-12-03 00:54:06 -0600

    add: learn about constants in java

--- src/variables/TypeConst.java
+++ src/variables/TypeConst.java
@@ -1,4 +1,18 @@
 package variables;
 
 public class TypeConst {
+    public static void main(String[] args) {
+        System.out.println("<-- CONSTANTES EN JAVA -->");
+        final var DAYS_WEEK = 7;
+        System.out.println("DAYS_WEEK = " + DAYS_WEEK);
+        //DAYS_WEEK = 8; -> !cannot assign a value to final variable 'DAYS_WEEK'
+        final var PI = 3.1416;
+        System.out.println("PI = " + PI);
+
+        System.out.println("Math.pi = " + Math.PI); // <- YA ESTA DEFINIDA DE LA CLASE MATH
+        final var MESSAGE = "Bienvenido jeje";
+        System.out.println("MESSAGE = " + MESSAGE);
+        final var MINUTES_PER_SECCONDS = 60;
+        System.out.println("MINUTES_PER_SECCONDS = " + MINUTES_PER_SECCONDS);
+    }
 }
