d0d3d91138bccd36ca16e2e6e9b3a71e6f8a34e9
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-12-03 00:53:24 -0600

    add: basic concat string

---
+++ src/variables/ConcatStrings.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class ConcatStrings {
+    public static void main(String[] args) {
+        System.out.println("<-- CONCATENACION DE CADENAS -->");
+        var name = "Jazzo";
+        var lastname = "ródriguez López";
+        var fullName = name + " " + lastname;
+
+        //ESTE EJERCICIO ES BASICO PARA ENTRAR AL TEMA DE MANEJO DE CADENAS
+        System.out.println("fullName = " + fullName);
+        System.out.println("fullName = " + fullName);
+    }
+}
