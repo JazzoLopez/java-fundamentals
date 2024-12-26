af0c00c3b0c915868dfd89a42c8289ba751e771d
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-12-25 18:21:02 -0600

    add: going through the chain

--- src/cadenas/strings.java
+++ src/cadenas/strings.java
@@ -3,19 +3,11 @@ package cadenas;
 public class strings {
     public static void main(String[] args) {
         System.out.println("<- MANEJO DE CADENAS ->");
-        var cadena1 = "Hola";
+        var cadena1 = "Hola mundo";
         System.out.println("cadena1 = " + cadena1);
-        var cadena2 = new String("Mundo");
-        System.out.println("cadena2 = " + cadena2);
-        var cadena3 = cadena1 + " " + cadena2;
-        System.out.println("cadena3 = " + cadena3);
-        //TEXT LONG
-        var cadena4 = """
-                Hola, este es
-                un texto
-                multilionea
-                """;
-
-        System.out.println("cadena4 = " + cadena4);
+        int lng = cadena1.length();
+        for(int i = 0; i < lng;i++){
+            System.out.println(cadena1.charAt(i));
+        }
     }
 }
